d10876ca4c25c69e9891c66264b6d65328cb975a
Bartek Bugała <bartek.bugala@gmail.com>
2019-06-11 22:10:36 +0200

    modified:   index.js

--- Zadanie-17.3/basic/index.js
+++ Zadanie-17.3/basic/index.js
@@ -1,22 +1,23 @@
 "use strict";
 process.stdin.setEncoding("utf-8");
-process.stdin.on("readable", function() {
+process.stdin.on("readable", function () {
   let input = process.stdin.read();
-  let instruction = input.toString().trim();
-
-  switch (instruction) {
-    case "/exit":
-      process.stdout.write("Quitting app!\n\n");
-      process.exit();
-      break;
-    case "/sys-lang":
-      process.stdout.write(process.env.LANG + "\n\n");
-      break;
-    case "/node-ver":
-      process.stdout.write(process.version + "\n\n");
-      break;
-    default:
-      process.stderr.write("Wrong instruction!\n\n");
-      break;
+  if (input !== null) {
+    let instruction = input.toString().trim();
+    switch (instruction) {
+      case "/exit":
+        process.stdout.write("Quitting app!\n\n");
+        process.exit();
+        break;
+      case "/sys-lang":
+        process.stdout.write(process.env.LANG + "\n\n");
+        break;
+      case "/node-ver":
+        process.stdout.write(process.version + "\n\n");
+        break;
+      default:
+        process.stderr.write("Wrong instruction!\n\n");
+        break;
+    };
   }
 });
