706ec2d43118287b09b9a0ed81836d2870720f51
Bartek Bugała <bartek.bugala@gmail.com>
2019-06-14 22:11:53 +0200

    modified:   basic/app/program.js

--- Zadanie-17.6/basic/app/program.js
+++ Zadanie-17.6/basic/app/program.js
@@ -1,11 +1,23 @@
 'use strict';
+const EventEmitter = require('events').EventEmitter;
+const emitter = new EventEmitter();
+
 const osInfo = require('../modules/osInfo');
+
+emitter.on('beforeCommand', function(instruction) {
+  console.log('You wrote: ' + instruction + ' trying ot run command.');
+});
+
+emitter.on('afterCommand', function() {
+  console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
   let input = process.stdin.read();
   if (input !== null) {
     let instruction = input.toString().trim();
+    emitter.emit('beforeCommand', instruction);
     switch (instruction) {
       case '/exit':
         process.stdout.write('Quitting app!\n\n');
@@ -24,5 +36,6 @@ process.stdin.on('readable', function() {
         process.stderr.write('Wrong instruction!\n\n');
         break;
     }
+    emitter.emit('afterCommand');
   }
 });
