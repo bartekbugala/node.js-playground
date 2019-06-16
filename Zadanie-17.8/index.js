7c0e13e621f7b45ec8abcf2f8a9ed165e18e9d9a
Bartek Bugała <bartek.bugala@gmail.com>
2019-06-16 23:59:05 +0200

    modified:   Zadanie-17.8/index.js

--- Zadanie-17.8/index.js
+++ Zadanie-17.8/index.js
@@ -1,9 +1,10 @@
 'use strict'
 const http = require('http');
 
-const server = http.createServer(function (request, response) {
-
+const server = http.createServer();
+server.on('request', function (request, response) {
+    response.write('Hello - response');
+    response.end();
 });
 
-
-
+server.listen(9000);
