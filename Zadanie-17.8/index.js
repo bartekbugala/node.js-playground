4dd39fecc568954d2659ed6a407593a31624c2f4
Bartek Bugała <bartek.bugala@gmail.com>
2019-06-17 01:57:28 +0200

    modified:   index.js

--- Zadanie-17.8/index.js
+++ Zadanie-17.8/index.js
@@ -1,10 +1,18 @@
-'use strict'
+'use strict';
 const http = require('http');
-
 const server = http.createServer();
+
 server.on('request', function (request, response) {
-    response.write('Hello - response');
-    response.end();
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+        response.write('<h1>Hello - response</h1>');
+        response.end();    
+    }  else {
+        response.statusCode = 404;
+        response.write('<h1>404: Wrong Path!</h1>');
+        response.end
+    }
+
 });
 
-server.listen(9000);
+server.listen(8080);
