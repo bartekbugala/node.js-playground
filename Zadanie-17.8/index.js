b1c119420ad50e082f664d6b49edefe5ca5ec867
Bartek.B <bartek.bugala@gmail.com>
2019-06-17 13:24:39 +0200

    modified:   index.js

--- Zadanie-17.8/index.js
+++ Zadanie-17.8/index.js
@@ -1,26 +1,38 @@
 'use strict';
+const path = require('path');
 const http = require('http');
 const fs = require('fs');
+
 
 const indexFile = './index.html';
 const imageFile = './error.jpg';
 const server = http.createServer();
 
-server.on('request', function(request, response) {
-  if (request.method === 'GET' && request.url === '/s') {
-    fs.readFile(indexFile, 'utf-8', function(err, data) {
-      if (err) throw err;
-      response.setHeader('Content-Type', 'text/html; charset=utf-8');
-      response.write(data);
-      response.end();
-    });
-  } else {
-    fs.readFile(imageFile, function(err, data) {
-      if (err) throw err;
-      response.setHeader('Content-Type', 'image/jpeg');  
-      response.end(data, 'binary');
-    });
-  }
+server.on('request', function (request, response) {
+  
+    if (request.method === 'GET' && request.url === '/') {
+        responseWithFile(indexFile, request, response);    
+    } else {
+        responseWithFile(imageFile, request, response);
+    }
 });
 
 server.listen(8080);
+
+function responseWithFile(file, request, response) {  
+    fs.readFile(file, function (err, data) {
+        if (err) throw err;
+        let ctype;    
+        if (path.extname === '.html') {         
+            ctype = 'text/html; charset=utf-8';
+            response.setHeader('Content-Type', ctype);
+            response.write(data);
+            response.end();      
+        }
+        else {    
+            ctype = 'Content-Type', 'image/jpeg'
+            response.setHeader('Content-Type', ctype);
+            response.end(data, 'binary');         
+        }
+    });
+}
