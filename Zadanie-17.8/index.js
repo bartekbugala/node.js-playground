8892270877b5b4129750bea1dd5e2360aa20fca0
Bartek.B <bartek.bugala@gmail.com>
2019-06-17 11:19:11 +0200

    modified:   Zadanie-17.8/index.js

--- Zadanie-17.8/index.js
+++ Zadanie-17.8/index.js
@@ -1,8 +1,7 @@
 'use strict';
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
 const http = require('http');
 const fs = require('fs');
+
 const indexFile = './index.html';
 const imageFile = './error.jpg';
 const server = http.createServer();
@@ -16,24 +15,12 @@ server.on('request', function(request, response) {
       response.end();
     });
   } else {
-   // fs.readFile(imageFile, undefined, function(err, data) {
-   //   if (err) throw err;
-      response.setHeader('Content-Type', 'image/jpeg');
-      response.end( returnDataFromFile(imageFile), 'binary');
-   // });
+    fs.readFile(imageFile, function(err, data) {
+      if (err) throw err;
+      response.setHeader('Content-Type', 'image/jpeg');  
+      response.end(data, 'binary');
+    });
   }
 });
 
 server.listen(8080);
-
-function returnDataFromFile(file) {
-    let fileData;
-    fs.readFile(file, undefined, function(err, data) {
-        if (err) throw err;
-        fileData = data;
-        emitter.emit('dataReady');
-});
-emitter.on('dataReady', function() {
-    return fileData;
-})
-}
