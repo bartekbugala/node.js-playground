d29eaac6cda4a6b733d6487a0e31b79244154683
Bartek Bugała <bartek.bugala@gmail.com>
2019-06-17 02:40:26 +0200

    new file:   error.jpg 	new file:   index.html 	modified:   index.js

--- Zadanie-17.8/index.js
+++ Zadanie-17.8/index.js
@@ -1,18 +1,25 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const indexFile = './index.html';
+const imageFile = './error.jpg';
 const server = http.createServer();
 
-server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/') {
-        response.write('<h1>Hello - response</h1>');
-        response.end();    
-    }  else {
-        response.statusCode = 404;
-        response.write('<h1>404: Wrong Path!</h1>');
-        response.end
-    }
-
+server.on('request', function(request, response) {
+  if (request.method === 'GET' && request.url === '/') {
+    fs.readFile(indexFile, 'utf-8', function(err, data) {
+      if (err) throw err;
+      response.setHeader('Content-Type', 'text/html; charset=utf-8');
+      response.write(data);
+      response.end();
+    });
+  } else {
+    fs.readFile(imageFile, function(err, data) {
+      if (err) throw err;
+      response.setHeader('Content-Type', 'image/jpeg' );
+      response.end(data, 'binary');
+    });
+  }
 });
 
 server.listen(8080);
